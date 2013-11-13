6b7d7e3ae217520b14668506d259b793da8ce5a4
Andrew Guard <andrew.s.guard@gmail.com>
2013-11-12 19:08:29 -0600

    changed environment

--- config/environment.rb
+++ config/environment.rb
@@ -35,12 +35,12 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
-if development?
-  env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
-  env_config.each do |key, value|
-    ENV[key] = value
-  end
-end
+# if development?
+#   env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+#   env_config.each do |key, value|
+#     ENV[key] = value
+#   end
+# end
 
 Twitter.configure do |config|
   config.consumer_key = ENV['TWITTER_KEY']
